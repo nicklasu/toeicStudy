4138abc528e4e2109eedc3ab189e791fd41d04ba
nikke <nicklass84@gmail.com>
2024-02-06 16:00:11 +0900

    eslint

--- src/app/components/AnswerButton.tsx
+++ src/app/components/AnswerButton.tsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 export const AnswerButton = ({ label, onClick, disabled }) => {
-    const [isButtonDisabled, setButtonDisabled] = useState(false);
-      const handleClick = () => {
-        onClick();
-    setButtonDisabled(true);
-  };
-    useEffect(() => {
-      if (disabled) {
+  const [isButtonDisabled, setButtonDisabled] = useState(false)
+  const handleClick = () => {
+    onClick()
     setButtonDisabled(true)
-      } else {
-        setButtonDisabled(false)
-      }
-    }, [disabled])
-    
+  }
+  useEffect(() => {
+    if (disabled) {
+      setButtonDisabled(true)
+    } else {
+      setButtonDisabled(false)
+    }
+  }, [disabled])
+
   return (
       <button className="bg-gray-300 hover:bg-gray-600 font-bold py-2 px-4 rounded" onClick={handleClick} disabled={isButtonDisabled}>
       {label}
     </button>
-  );
-};
+  )
+}
