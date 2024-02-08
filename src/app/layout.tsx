b90cc9e1acfe7fa73ac8d23faa8595d9b0570390
nikke <nicklass84@gmail.com>
2024-02-08 15:15:26 +0900

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import React from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
